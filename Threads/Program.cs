ad744816eff82bc3f539af47a100132212662262
Avinash Tauro <avinash.tauro@arctechinfo.com>
2022-01-25 12:20:08 +0530

    updated document and added a new example

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -6,11 +6,13 @@ namespace Threads
     {
         static void Main(string[] args)
         {
+            ThreadExample.Demo();
+
             //NoThreadDemo.Demo();
             //SimpleThreadDemo.Demo();
             //SimpleThreadDemo2.Demo();
             //SimpleThreadDemo3.Demo();
-            SimpleThreadDemo4Background.Demo();
+            //SimpleThreadDemo4Background.Demo();
         }
     }
 }
---
+++ Threads/ThreadExample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    internal class ThreadExample
+    {
+        static void Show()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                Console.Write($"A{i} ");
+            }
+        }
+
+        static void Jump()
+        {
+            for (int i = 1; i <= 500; i++)
+            {
+                Console.Write($"B{i} ");
+            }
+        }
+
+        public static void Demo()
+        {
+            Thread t1 = new Thread(Show); // by default foreground thread
+            Thread t2 = new Thread(Show); // by default foreground thread
+            //t1.IsBackground = true;
+            //t2.IsBackground = true;
+
+            t1.Start();
+            t2.Start();
+
+            for (int i = 1; i <= 500; i++)
+            {
+                Console.Write($"C{i} ");
+            }
+        }
+    }
+}
+
